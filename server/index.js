e1599133b812c7cfed3f5db776774a2422695996
Villalba <lmv@f84d899822fc.ant.amazon.com>
2022-08-18 14:53:48 +0300

    remove unnecessary code

--- server/index.js
+++ server/index.js
@@ -39,18 +39,6 @@ app.use('/api/store', require('./routes/store'));
 //https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
 app.use('/uploads', express.static('uploads'));
 
-// SERVE STATIC FILES
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-	// Set static folder
-	app.use(express.static('client/build'));
-
-	// index.html for all page routes
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-	});
-}
-
 // START SERVER
 const port = process.env.PORT || 8080;
 
